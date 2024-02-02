b2db3495fbd923e9aef765c9f047e399a5f0d488
Justin Zak <irotsomadev@gmail.com>
2024-02-02 14:13:41 -0800

    Changed major.minor version to 1.0 instead of 0.1 and bumped it one incremental version to get an install. This should solve the issue with octoprint thinking there's a newer version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ plugin_package = "octoprint_psucontrol_momentarygpio"
 plugin_name = "OctoPrint-PSUControl-MomentaryGpio"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "1.0.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
